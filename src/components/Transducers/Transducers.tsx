26497db3740293498c6fd5127c5dd8609a21b581
MattAKeider <kderr41@gmail.com>
2024-01-19 17:40:04 -0500

    added empty screen state

--- src/components/Transducers/Transducers.tsx
+++ src/components/Transducers/Transducers.tsx
@@ -1,10 +1,14 @@
 import { useState, useRef, useContext } from 'react';
 
-import { TransducerContext, TransducerContextType } from '../../store/transducer-context';
+import {
+  TransducerContext,
+  TransducerContextType,
+} from '../../store/transducer-context';
 import Modal, { ModalHandle } from '../../ui/Modal/Modal';
 import { Transducer } from '../../data/data';
 import TransducerItem from '../TransducerItem/TransducerItem';
 import FullDetails from '../FullDetails/FullDetails';
+import EmptyScreen from '../EmptyScreen/EmptyScreen';
 import styles from './Transducers.module.css';
 
 const Transducers = () => {
@@ -17,7 +21,7 @@ const Transducers = () => {
     modalRef.current.open();
   };
 
-  const handleDeleteTransducer = (id: string, name: string, event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleDeleteTransducer = (id: string, name: string, event: React.MouseEvent<SVGAElement>) => {
     event.stopPropagation();
 
     if (confirm(`Are you sure you would like to delete ${name}?`)) {
@@ -29,21 +33,36 @@ const Transducers = () => {
     modalRef.current.close();
   };
 
+  let content: JSX.Element = (
+    <ul className={styles.container}>
+      {transducers.map((transducer: Transducer) => (
+        <TransducerItem
+          key={transducer.id}
+          transducerData={transducer}
+          onClickTransducer={() => handleClickedTransducer(transducer)}
+          onClickDelete={(event) =>
+            handleDeleteTransducer(transducer.id, transducer.name, event)
+          }
+        />
+      ))}
+    </ul>
+  );
+
+  if (transducers.length === 0) {
+    content = <EmptyScreen />
+  }
+
   return (
     <>
       <Modal ref={modalRef}>
-        {selectedTransducer && <FullDetails transducer={selectedTransducer} onCloseModal={handleCloseDetails} />}
-      </Modal>
-      <ul className={styles.container}>
-        {transducers.map((transducer: Transducer) => (
-          <TransducerItem
-            key={transducer.id}
-            transducerData={transducer}
-            onClickTransducer={() => handleClickedTransducer(transducer)}
-            onClickDelete={(event) => handleDeleteTransducer(transducer.id, transducer.name, event)}
+        {selectedTransducer && (
+          <FullDetails
+            transducer={selectedTransducer}
+            onCloseModal={handleCloseDetails}
           />
-        ))}
-      </ul>
+        )}
+      </Modal>
+      {content}
     </>
   );
 };
