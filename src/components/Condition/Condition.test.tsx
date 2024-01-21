e0e657f2125a19771a6b9432b1bac3433b14c90c
MattAKeider <kderr41@gmail.com>
2024-01-21 12:52:18 -0500

    fixed condition component tests

--- src/components/Condition/Condition.test.tsx
+++ src/components/Condition/Condition.test.tsx
@@ -8,18 +8,32 @@ const testData: TransducerCondition = {
   conditionId: crypto.randomUUID(),
   condition: 'Working',
   conditionChangedDate: new Date('2024-01-8'),
+  note: 'This transducer is brand new from the factory.',
 };
 
 describe('Condition', () => {
   test('should render the transducer condition', () => {
     render(<Condition transducerCondition={testData}/>);
-    const condition = screen.getByTestId('condition');
-    expect(condition).toBeInTheDocument();
+    const p = screen.getByTestId('condition');
+    expect(p).toBeInTheDocument();
   });
 
   test('should format the date correctly', () => {
     render(<Condition transducerCondition={testData}/>);
-    const date = screen.getByTestId('date');
-    expect(date).toHaveTextContent('Jan 8, 2024');
+    const p = screen.getByTestId('date');
+    expect(p).toHaveTextContent('Jan 8, 2024');
+  });
+
+  test('should render the transducer note if given', () => {
+    render(<Condition transducerCondition={testData}/>);
+    const p = screen.getByTestId('saved-note');
+    expect(p).toHaveTextContent('This transducer is brand new from the factory.');
+  });
+
+  test('should not render the transducer note if empty', () => {
+    testData.note = '';
+    render(<Condition transducerCondition={testData}/>);
+    const p = screen.queryByTestId('saved-note');
+    expect(p).not.toBeInTheDocument();
   });
 });
