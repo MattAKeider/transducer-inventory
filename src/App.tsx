99d4534b57f715ef67e33ec73aecea9bf5b3222a
MattAKeider <kderr41@gmail.com>
2024-02-23 22:58:39 -0500

    added dummy login route for later use

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MessagePage from './components/MessagePage/MessagePage';
 import Root from './pages/Root';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {
@@ -10,7 +11,8 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <MessagePage message='Page cound not be found!' />,
     children: [
-      { path: '/', element: <Home /> }
+      { path: '/', element: <Home /> },
+      { path: '/login', element: <Login />}
     ]
   }
 ]);
