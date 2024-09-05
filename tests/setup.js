dfcef8ac3266a3d7a789ef40525b79938ac2df25
MattAKeider <kderr41@gmail.com>
2024-09-05 18:32:42 -0400

    fix lint error

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,10 @@
 /*global document*/
 /*eslint no-undef: "error"*/
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
