d4eaf65a5012d98cd9516053642f3145b8878ae8
MattAKeider <kderr41@gmail.com>
2024-09-16 21:25:15 -0400

    add tests for transducer context

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom/vitest';
+
+import { cleanup } from '@testing-library/react';
+
+beforeEach(() => {
+  // Test environment doesn't know where the modal is within index.html
+  const modalEl = document.createElement('div');
+  modalEl.setAttribute('id', 'modal');
+  document.body.appendChild(modalEl);
+});
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  document.body.querySelector('#modal').remove();
+  cleanup();
+});
