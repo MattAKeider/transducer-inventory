1ed60514bec15c803d5b93ecb0d69cbd334e70e0
MattAKeider <kderr41@gmail.com>
2024-10-13 20:58:29 -0400

    added button component tests

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -2,11 +2,13 @@ import styles from './Button.module.css';
 
 interface Props extends React.ComponentPropsWithoutRef<"button"> {
   children: React.ReactNode;
+  type?: 'submit' | 'reset' | 'button';
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
-const Button = ({children, ...props}: Props) => {
+const Button = ({children, type = 'button', onClick, ...props}: Props) => {
   const classes = `${styles.button} ${props.className}`;
-  return <button {...props} className={classes}>{children}</button>;
+  return <button type={type} onClick={onClick} {...props} className={classes}>{children}</button>;
 };
 
 export default Button;
