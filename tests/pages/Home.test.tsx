4c2f526fbd7b94eb53cf51f6ca769225945c7b50
MattAKeider <kderr41@gmail.com>
2024-10-29 13:39:03 -0400

    added component tests for home component page

---
+++ tests/pages/Home.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import { http, HttpResponse } from 'msw';
+
+import TransducerContextProvider from '../../src/context/TransducerContext';
+import { server } from '../data/server';
+import Home from "../../src/pages/Home";
+
+describe('Home', () => {
+  test('should render the Home component', async () => {
+    render(
+      <TransducerContextProvider>
+        <Home />
+      </TransducerContextProvider>
+    );
+
+    expect(await screen.findByLabelText('circles-loading')).not.toBeVisible();
+    
+    const list = screen.getByRole('list');
+    const items = await screen.findAllByRole('listitem');
+    
+    expect(list).toBeInTheDocument();
+    expect(items.length).toBeGreaterThan(1);
+  });
+
+  test('should render MessagePage component on error', async () => {
+    server.use(
+      http.get('http://localhost:5000/api/transducers', () => {
+        return HttpResponse.json({message: 'Internal Server Error'}, {status: 500});
+      })
+    );
+
+    render(
+      <TransducerContextProvider>
+        <Home />
+      </TransducerContextProvider>
+    );
+
+    expect(await screen.findByLabelText('circles-loading')).not.toBeVisible();
+    expect(screen.getByRole('heading')).toHaveTextContent(/something went wrong/i);
+  });
+});
